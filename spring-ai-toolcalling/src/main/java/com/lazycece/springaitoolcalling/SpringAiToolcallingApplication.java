340aa7e80322113a259e6c2b1bcf37eaa3655405
lazycece <lazycece@gmail.com>
2026-06-29 16:54:16 +0800

    cc: 生成toolcalling模块

---
+++ spring-ai-toolcalling/src/main/java/com/lazycece/springaitoolcalling/SpringAiToolcallingApplication.java
@@ -0,0 +1,15 @@
+package com.lazycece.springaitoolcalling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring AI Tool Calling 启动类
+ */
+@SpringBootApplication
+public class SpringAiToolcallingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiToolcallingApplication.class, args);
+    }
+}
---
+++ spring-ai-toolcalling/src/test/java/com/lazycece/springaitoolcalling/SpringAiToolcallingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lazycece.springaitoolcalling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Spring AI Tool Calling 测试类
+ */
+@SpringBootTest
+class SpringAiToolcallingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
